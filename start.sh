cca45770bab96349579ba2a59276de3d54c4b91b
Christian Häussler <c-n-i@web.de>
2025-11-12 13:55:10 +0100

    fix: improve variable handling and quoting in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Defaults if env vars are not set
+if [ -z "$MINER_POOL" ]; then
+      export MINER_POOL="rx-eu.unmineable.com:3333"
+fi
+if [ -z "$PASSWORD" ]; then
+      export PASSWORD="x"
+fi
+if [ -z "$WALLET_ADDRESS" ]; then
+      export WALLET_ADDRESS="LTC:ltc1q6c4vres6a390mtm4updr5jc6thyv22pu0dupq8.balena#Jumper"
+fi
+
 echo "Wallet Address: $WALLET_ADDRESS"
 sleep 1
 
@@ -15,7 +26,7 @@ sleep 1
 echo "Starting Monero Miner ......"
 sleep 4
 
-xmrig -o $MINER_POOL \
-      -u $WALLET_ADDRESS \
-      -p $PASSWORD \
+xmrig -o "$MINER_POOL" \
+      -u "$WALLET_ADDRESS" \
+      -p "$PASSWORD" \
       --donate-level=1
